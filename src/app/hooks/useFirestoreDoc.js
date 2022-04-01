c4e06b39df15e6eefb437c8495e896c2dc0a8dda
kodaishiotsuki <0711dr5@gmail.com>
2022-04-01 16:36:23 +0900

    matchub引継ぎ

---
+++ src/app/hooks/useFirestoreDoc.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  asyncActionError,
+  asyncActionFinish,
+  asyncActionStart,
+} from "../async/asyncReducer";
+import { dataFromSnapshot } from "../firestore/firestoreService";
+
+//firestoreのDB取得→表示
+export default function useFirestoreDoc({
+  query,
+  data,
+  deps,
+  shouldExecute = true,
+}) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!shouldExecute) return;
+    dispatch(asyncActionStart());
+    const unsubscribe = query().onSnapshot(
+      (snapshot) => {
+        //特定のページ以外のアクセス対応
+        if (!snapshot.exists) {
+          dispatch(
+            asyncActionError({
+              code: "not-found",
+              message: "Could not find document",
+            })
+          );
+          return;
+        }
+        data(dataFromSnapshot(snapshot));
+        dispatch(asyncActionFinish());
+      },
+      (error) => dispatch(asyncActionError(error))
+    );
+    return () => {
+      unsubscribe();
+    };
+  }, deps); //eslint-disable-line react-hooks/exhaustive-deps
+}
