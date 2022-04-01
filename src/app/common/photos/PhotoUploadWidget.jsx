d37bc597431884d0f972a8c84e026d74694b3abe
kodaishiotsuki <0711dr5@gmail.com>
2022-04-01 18:48:18 +0900

    firebase v8 not compat

--- src/app/common/photos/PhotoUploadWidget.jsx
+++ src/app/common/photos/PhotoUploadWidget.jsx
@@ -10,45 +10,49 @@ import { updateUserProfilePhoto } from "../../firestore/firestoreService";
 
 export default function PhotoUploadWidget({ setEditMode }) {
   const [files, setFiles] = useState([]);
-  const [image, setImage] = useState(null);
+  const [cropper, setCropper] = useState(null);
   const [loading, setLoading] = useState(false);
 
   //画像アップロード
   function handleUploadImage() {
     setLoading(true);
     const filename = cuid() + "." + getFileExtension(files[0].name);
-    const uploadTask = uploadToFirebaseStorage(image, filename);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-      },
-      (error) => {
-        toast.error(error.message);
-      },
-      () => {
-        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-          updateUserProfilePhoto(downloadURL, filename)
-            .then(() => {
-              setLoading(false);
-              handleCancelCrop();
-              setEditMode(false);
-            })
-            .catch((error) => {
-              toast.error(error.message);
-              setLoading(false);
-            });
-        });
-      }
-    );
+
+    cropper.getCroppedCanvas().toBlob(image => {
+      const uploadTask = uploadToFirebaseStorage(image, filename);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error) => {
+          toast.error(error.message);
+        },
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            updateUserProfilePhoto(downloadURL, filename)
+              .then(() => {
+                setLoading(false);
+                handleCancelCrop();
+                setEditMode(false);
+              })
+              .catch((error) => {
+                toast.error(error.message);
+                setLoading(false);
+              });
+          });
+        }
+      );
+    })
+    
   }
 
   //画像アップロードキャンセル
   function handleCancelCrop() {
     setFiles([]);
-    setImage(null);
+    setCropper(null);
   }
 
   return (
@@ -63,7 +67,7 @@ export default function PhotoUploadWidget({ setEditMode }) {
 
         {files.length > 0 && (
           <PhotoWidgetCropper
-            setImage={setImage}
+            setCropper={setCropper}
             imagePreview={files[0].preview}
           />
         )}
